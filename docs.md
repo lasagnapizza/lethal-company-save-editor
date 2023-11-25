531fb97b9642392b6f3b816de0d698526d46e870
kinduff <kinduff@protonmail.com>
2023-11-25 03:33:04 +0100

    initial work

---
+++ docs.md
@@ -0,0 +1,63 @@
+# shipGrabbableItemIDs
+1: boombox
+10: shovel
+12: extension ladder
+13: tzp-inhalant
+14: walkie-talkie
+15: gun
+16: magic 7 ball
+17: airhornw
+18: bell
+19: big bolt
+20: bottles
+21: brush
+22: candy
+23: cash register
+24: chemical jug
+25: horn
+26: large axle
+27: teeth
+28: dust pan
+29: egg beater
+30: v-type engine
+31: golden cup
+32: fancy lamp
+33: painting
+34: plastic fish
+35: laser pointer
+36: gold bar
+37: hairdryer
+38: magnifying glass
+39: metal sheet
+40: cookie mold pan
+41: mug
+42: perfume bottle
+43: old phone
+44: jar of pickles
+45: pill bottle
+46: remote
+47: ring
+48: toy robot
+49: rubber ducky
+5: key
+50: red soda
+51: steering wheel
+52: stop sign
+53: tea kettle
+54: toothpaste
+55: toy cube
+55: toy cube
+56: hive
+57: radar booster
+58: yield sign
+
+# CurrentPlanetID
+0: Experimentation
+1: Assurance
+2: Vow
+3: Company Building
+4: March
+5: Rend
+6: Dine
+7: Offense
+8: Titan
---
+++ main.rb
@@ -0,0 +1,80 @@
+require 'openssl'
+require 'json'
+
+def create_cipher(mode, password, salt, iter = 100, key_len = 16, digest = 'SHA1')
+  cipher = OpenSSL::Cipher.new('AES-128-CBC')
+  cipher.send(mode)
+  cipher.key = OpenSSL::PKCS5.pbkdf2_hmac(password, salt, iter, key_len, digest)
+  cipher.iv = salt if mode == :decrypt # For decryption, IV is the salt
+  cipher
+end
+
+def decrypt_aes(encrypted_data, password)
+  iv = encrypted_data[0..15] # Extract IV from the start of the data
+  cipher = create_cipher(:decrypt, password, iv)
+  encrypted_data = encrypted_data[16..-1] # The rest is the encrypted data
+  cipher.update(encrypted_data) + cipher.final
+rescue OpenSSL::Cipher::CipherError => e
+  puts "Error decrypting data: #{e.message}"
+  nil
+end
+
+def encrypt_aes(data, password)
+  cipher = OpenSSL::Cipher.new('AES-128-CBC')
+  cipher.encrypt
+  iv = cipher.random_iv # Generate a random IV for encryption
+  cipher.key = OpenSSL::PKCS5.pbkdf2_hmac(password, iv, 100, 16, 'SHA1')
+  cipher.iv = iv
+  encrypted_data = cipher.update(data) + cipher.final
+  iv + encrypted_data # Prepend IV to encrypted data
+rescue OpenSSL::Cipher::CipherError => e
+  puts "Error encrypting data: #{e.message}"
+  nil
+end
+
+def read_save_file(file_path)
+  begin
+    File.binread(file_path)
+  rescue Errno::ENOENT => e
+    puts "Error reading file: #{e.message}"
+    nil
+  end
+end
+
+def write_to_file(file_path, data)
+  begin
+    File.open(file_path, 'wb') { |file| file.write(data) }
+    puts "Data written to #{file_path}"
+  rescue IOError => e
+    puts "Error writing to file: #{e.message}"
+  end
+end
+
+# Replace with your actual file paths and password
+input_file_path = "/mnt/c/Users/kindu/AppData/LocalLow/ZeekerssRBLX/Lethal Company/LCSaveFile3"
+output_file_path = "/mnt/c/Users/kindu/AppData/LocalLow/ZeekerssRBLX/Lethal Company/LCSaveFile3.decrypted"
+encrypted_output_file_path = "/mnt/c/Users/kindu/AppData/LocalLow/ZeekerssRBLX/Lethal Company/LCSaveFile3"
+password = "lcslime14a5"
+
+# Decrypting and processing the file
+encrypted_data = read_save_file(input_file_path)
+if encrypted_data
+  decrypted_data = decrypt_aes(encrypted_data, password)
+  if decrypted_data
+    puts "Decrypted Data: #{decrypted_data}"
+    write_to_file(output_file_path, decrypted_data)
+
+    # Prompt the user to modify the decrypted file and then press Enter
+    puts "Modify the decrypted file at #{output_file_path} then press Enter to continue."
+    gets # Waits for the user to press Enter
+
+    # Read the modified file
+    modified_data = read_save_file(output_file_path)
+    if modified_data
+      # Re-encrypt and save the modified data
+      re_encrypted_data = encrypt_aes(modified_data, password)
+      write_to_file(encrypted_output_file_path, re_encrypted_data) if re_encrypted_data
+    end
+  end
+end
+
