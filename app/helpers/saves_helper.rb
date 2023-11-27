66b33ee50e19629ed62f33855169e9c96ef9c609
kinduff <kinduff@protonmail.com>
2023-11-27 02:23:17 +0100

    Fixes scroll, improves form

--- app/helpers/saves_helper.rb
+++ app/helpers/saves_helper.rb
@@ -1,14 +1,5 @@
 module SavesHelper
   def form_field_for(form, save, field)
-    value = save.save_data[field]["value"]
-    case save.save_data[field]["__type"]
-    when "int"
-      form.number_field field.underscore, value: value
-    when "bool"
-      form.check_box field.underscore, checked: value
-    # Add more cases for other data types if needed
-    else
-      form.text_field field.underscore, value: value
-    end
+    form.text_field field.underscore, value: save.save_data[field]["value"]
   end
 end
